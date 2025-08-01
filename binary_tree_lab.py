e9b5690f3038a7bc3c90c0566bf2f38add3434a7
bblair321 <bblair321@yahoo.com>
2025-08-01 08:19:37 -0700

    all tests are passing

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -1,15 +1,30 @@
 from typing import Optional
 
 class TreeNode:
-    def __init__(self, val: int):
+    def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
         self.val = val
-        self.left: Optional['TreeNode'] = None
-        self.right: Optional['TreeNode'] = None
+        self.left = left
+        self.right = right
 
-# TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    """
+    Returns the maximum depth of a binary tree.
+    Depth is defined as the number of nodes along the longest path from root down to the farthest leaf node.
+    """
+    if root is None:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
 
-# TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    """
+    Returns the lowest common ancestor of two nodes p and q in a BST.
+    Assumes all values in the BST are unique.
+    """
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    elif p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+    else:
+        return root
